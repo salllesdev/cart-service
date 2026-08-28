00076e235b0a3eb284eb162b14d5260a9410cd25
G. Sales <gustavosales0809@gmail.com>
2026-08-27 23:02:43 -0300

    feat: implementing Serializable at ClientProductResponse

--- src/main/java/dev/salllesdev/cartservice/DTO/ClientProductResponse.java
+++ src/main/java/dev/salllesdev/cartservice/DTO/ClientProductResponse.java
@@ -1,6 +1,7 @@
 package dev.salllesdev.cartservice.DTO;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
-public record ClientProductResponse(Long id, String title, BigDecimal price) {
+public record ClientProductResponse(Long id, String title, BigDecimal price) implements Serializable {
 }
