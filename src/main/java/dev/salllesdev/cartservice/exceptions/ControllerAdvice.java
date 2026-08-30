a15c6a63e6dc1b063e86835c35242e7d4db1624b
G. Sales <gustavosales0809@gmail.com>
2026-08-30 17:50:53 -0300

    feat: ControllerAdivice

---
+++ src/main/java/dev/salllesdev/cartservice/exceptions/ControllerAdvice.java
@@ -0,0 +1,17 @@
+package dev.salllesdev.cartservice.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+
+    @ExceptionHandler(DataNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String HandleDataNotFoundException(DataNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+}
