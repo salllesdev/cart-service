cd32e1784f2ca7bb18ad0a09e5cff6e69a54a6ca
G. Sales <gustavosales0809@gmail.com>
2026-09-01 19:43:11 -0300

    feat: add ResponseEntity to ProductController endpoints

--- src/main/java/dev/salllesdev/cartservice/controller/ProductController.java
+++ src/main/java/dev/salllesdev/cartservice/controller/ProductController.java
@@ -3,6 +3,7 @@ package dev.salllesdev.cartservice.controller;
 import dev.salllesdev.cartservice.DTO.ClientProductResponse;
 import dev.salllesdev.cartservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +19,14 @@ public class ProductController {
     private final ProductService service;
 
     @GetMapping("/")
-    public List<ClientProductResponse> getAll() {
-        return service.getAll();
+    public ResponseEntity<List<ClientProductResponse>> getAll() {
+        List<ClientProductResponse> list = service.getAll();
+        return ResponseEntity.ok(list);
     }
 
     @GetMapping("/{id}")
-    public ClientProductResponse getById(@PathVariable Long id) {
-        return service.getById(id);
+    public ResponseEntity<ClientProductResponse> getById(@PathVariable Long id) {
+        ClientProductResponse product = service.getById(id);
+        return ResponseEntity.ok(product);
     }
 }
